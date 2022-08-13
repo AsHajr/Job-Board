1d260d68f82e37a2af8577029cd9f80d4d08eb14
AsHajr <asmawhajeer@gmail.com>
2022-08-13 14:50:21 +0300

    added user model & authentication

---
+++ spec/requests/authentication_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Authentication', type: :request do
+  describe 'POST /auth/login' do
+    let!(:user) { create(:user) }
+    let(:headers) { valid_headers.except('Authorization') }
+    let(:valid_credentials) do
+      {
+        email: user.email,
+        password: user.password
+      }.to_json
+    end
+    let(:invalid_credentials) do
+      {
+        email: "test@email.com",
+        password: "password"
+      }.to_json
+    end
+
+
+    context 'When request is invalid' do
+      before { post '/auth/login', params: invalid_credentials, headers: headers }
+
+      it 'returns a failure message' do
+        expect(json['message']).to match(/Invalid credentials/)
+      end
+    end
+  end
+end
