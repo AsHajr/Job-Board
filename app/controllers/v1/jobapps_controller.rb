bc1e484b4e135ff243d8b52e74a84dbdea38790e
AsHajr <asmawhajeer@gmail.com>
2022-08-14 16:31:59 +0300

    added versioning

---
+++ app/controllers/v1/jobapps_controller.rb
@@ -0,0 +1,36 @@
+module V1
+    class JobappsController < ApplicationController
+        before_action :set_job
+        before_action :set_job_jobapp, only: [:show]
+
+        # GET /jobs/:job_id/jobapps
+        def index
+            json_response(@job.jobapps)
+        end
+
+        # GET /jobs/:job_id/jobapps/:id
+        def show
+            json_response(@jobapp)
+        end
+
+        # POST /jobs/:job_id/jobapps
+        def create
+            @job.jobapps.create!(jobapp_params)
+            json_response(@job, :created)
+        end
+        
+        private
+
+        def jobapp_params
+            params.permit(:created_by)
+        end
+
+        def set_job
+            @job = Job.find(params[:job_id])
+        end
+
+        def set_job_jobapp
+            @jobapp = @job.jobapps.find_by!(id: params[:id]) if @job
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 
-  resources :jobs do
-    resources :jobapps
+
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :jobs do
+      resources :jobapps
+    end
   end
 
 end
