d6da01e53189fb1f5bf16209118982c11a8690b2
AsHajr <asmawhajeer@gmail.com>
2022-08-20 18:40:51 +0300

    edit serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,6 +1,6 @@
 class JobSerializer < ActiveModel::Serializer
   # attributes to be serialized
-  attributes :id, :title, :description, :created_by, :created_at, :updated_at
+  attributes :id, :title, :description, :created_by, :created_at, :updated_at, :expiry_date
   # model association
   has_many :jobapps
 end
